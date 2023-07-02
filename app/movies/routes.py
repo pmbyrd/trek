da7519ae0e2e77e399ce64eea439658405cf6125
Perriesha <pmbyrd@gmail.com>
2023-07-01 22:34:15 -0700

    Changed the API to the omdb, better results

--- app/movies/routes.py
+++ app/movies/routes.py
@@ -4,6 +4,11 @@ from flask import render_template, request, jsonify
 from app.movies import movies
 from app import db
 from app.star_trek_models import Movie
+from flask_restful import Resource, Api
+from flask import jsonify
+from app.movies.schemas import MovieSchema
+
+
 
 @movies.route('/')
 def show_movies():
@@ -13,4 +18,19 @@ def show_movies():
     # must also send the movies back as json for the javascript to use
     return render_template('movies.html', movies=movies)
 
+# make a request to the backend to get the movies
+@movies.route('/get_movies')
+def get_movies():
+    """This page shows all the movies in the database."""
+        
+    movies = Movie.query.all()
+    # Serialize the movies using the MovieSchema
+    movie_schema = MovieSchema(many=True)
+    movies_json = movie_schema.dump(movies)
+    
+    return jsonify(movies=movies_json)
 
+@movies.route('/test')
+def testing():
+    """Test request to for json"""
+    return jsonify({'test': 'test'})
--- app/movies/static/js/movies.js
+++ app/movies/static/js/movies.js
@@ -1,26 +1,48 @@
 console.log('movies.js loaded');
 const API_KEY = "4cc3d718d79609d7b538246e87a080f0"
 const Base_URL = 'https://api.themoviedb.org/'
+const $moviesContainer = $(".movies")
+
+const OMDBAPI = "http://www.omdbapi.com/?i=tt3896198&apikey=cbdaf169"
+
+
 
 async function getMovies() {
     try {
         //this call is to my own backend
-        const res = await axios.get('/movies')
-        console.log(res.data)
+        const res = await axios.get('/movies/get_movies')
+        const titles = res.data.movies.map(movie => movie.title)
+        return titles
     } catch (error) {
         console.log(error)
     }
 }
 
-getMovies()
 
-async function getMovie(movie) {
+async function getMovie(movieTitle) {
     try {
-        const res = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${movie}&api_key=4cc3d718d79609d7b538246e87a080f0`)
-        console.table(res.data.results)
+        const res = await axios.get(`${OMDBAPI}&t=${movieTitle}`);
+        const movie = res.data
+        return movie = {
+            title: movie.Title,
+            year: movie.Year,
+            rated: movie.Rated,
+            released: movie.Released,
+            runtime: movie.Runtime,
+            genre: movie.Genre,
+            director: movie.Director,
+            metascore: movie.Metascore,
+            plot: movie.Plot,
+            poster: movie.Poster,
+        }
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
-getMovie("star trek")
+
+
+
+
+
+displayMovies()
