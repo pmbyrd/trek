886c65de5c9769d043eac6ae812f19851cfbf1d2
Perriesha <pmbyrd@gmail.com>
2023-08-01 22:35:22 -0700

    Animals is being paginated

--- app/__init__.py
+++ app/__init__.py
@@ -50,6 +50,8 @@ def create_app(config_class=Config):
     @app.route('/test/')
     def test_page():
         return '<h1>Testing the Flask Application Factory Pattern</h1>'
+    
+    
 
                 
     return app
