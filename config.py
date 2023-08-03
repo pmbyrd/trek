429c4036143503848ca138093798535c2e6fa7c0
Perriesha <pmbyrd@gmail.com>
2023-08-03 09:29:14 -0700

    Moving back to stapi-branch

--- config.py
+++ config.py
@@ -4,11 +4,20 @@ import psycopg2
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class Config:
+class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     OMDB_API_KEY = os.environ.get('OMDB_API_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')\
         or 'sqlite:///' + os.path.join(basedir, 'app.db') or 'postgresql:///trek'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-    #assign the database URI to the SQLALCHEMY_DATABASE_URI key
+#     #assign the database URI to the SQLALCHEMY_DATABASE_URI key
+# class ProductionConfig(Config):
+#     DATABASE_URI = 'mysql://user@localhost/foo'
+
+# class DevelopmentConfig(Config):
+#     DATABASE_URI = "sqlite:////tmp/foo.db"
+
+# class TestingConfig(Config):
+#     DATABASE_URI = 'sqlite:///trek.db'
+#     TESTING = True
